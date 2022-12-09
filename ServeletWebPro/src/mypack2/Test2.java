96c5e0d8b62a068c2054202afec7f8cdbb2e611e
Aishwarya Patle <aishwaryapatle22@gmail.com>
2022-12-09 15:05:39 +0530

    How to create a basic Servlet

---
+++ ServeletWebPro/src/mypack2/Test2.java
@@ -0,0 +1,27 @@
+package mypack2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**ii
+ * Servlet implementation class Test2
+ */
+@WebServlet("/test2")
+public class Test2 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("Welcome to <b>servelet 2</b>");
+	}
+
+	
+
+}
